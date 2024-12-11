81f20adb0f303b0bc5daf5f27fae85a831522881
Light54321 <99309736+Light54321@users.noreply.github.com>
2024-12-11 10:16:39 +0530

    just adding

---
+++ palindrom/palindrom_without_using_funtion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name[100], copy[100], d;
+    printf("Enter the Name:");
+    gets(name);
+    for(int i=0;name[i]!=\0;i++){
+
+
+    }
+    return 0;
+}
